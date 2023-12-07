e1d0c10216bc11229868821a58de3e7954997f89
buzzkim <xogud5053@hanmail.net>
2023-12-08 00:07:32 +0900

    feat: practict make chart

---
+++ coronaboard-web/src/pages/chart/google-table-chart.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import { Chart } from 'react-google-charts';
+
+export default function GoogleTableChart() {
+  const header = [
+    { type: 'string', label: '지역' },
+    { type: 'number', label: '확진자' },
+    { type: 'number', label: '사망자' },
+    { type: 'number', label: '격리해제' },
+  ];
+
+  const rows = [
+    ['서울', 22717, 277, 17487],
+    ['경기', 18378, 393, 14538],
+    ['대구', 8716, 206, 7787],
+  ];
+
+  const data = [header, ...rows];
+
+  return (
+    <Container>
+      <Chart
+        chartType="Table"
+        loader={<div>로딩 중</div>}
+        data={data}
+        options={{
+          showRowNumber: true,
+          allowHtml: true,
+          width: '100%',
+          height: '100%',
+        }}
+      />
+    </Container>
+  );
+}
