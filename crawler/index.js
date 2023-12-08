581d7e2a7fb8141052ddef339174bb79c23cf500
buzzkim <xogud5053@hanmail.net>
2023-12-09 05:12:55 +0900

    fix: modify crawler files

--- crawler/index.js
+++ crawler/index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const fs = require("fs");
 const ApiClient = require("./api-client");
 const { crawlAndUpdateDomestic } = require("./domestic-updater");
+const { crawlAndUpdateGlobal } = require("./global-updater");
 
 async function main() {
   const outputPath = path.join(process.cwd(), "output");
@@ -15,6 +16,13 @@ async function main() {
     await crawlAndUpdateDomestic(outputPath, apiClient);
   } catch (e) {
     console.error("crawlAndUpdateDomestic failed", e);
+  }
+
+  try {
+    console.log("crawlAndUpdateGlobal started");
+    await crawlAndUpdateGlobal(outputPath, apiClient);
+  } catch (e) {
+    console.error("crawlAndUpdateGlobal failed", e);
   }
 }
 
